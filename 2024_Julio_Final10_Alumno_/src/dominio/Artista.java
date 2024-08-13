593ff6461bf4b61ab2348ee9a967906d413cf5cc
Andres Marquez <aemb143@gmail.com>
2024-08-13 15:29:50 -0300

    Se agrego los getters y settes, y el metodo getEdad

--- 2024_Julio_Final10_Alumno_/src/dominio/Artista.java
+++ 2024_Julio_Final10_Alumno_/src/dominio/Artista.java
@@ -15,12 +15,52 @@ public class Artista {
 		
 	}
 	
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public GregorianCalendar getFechaNacto() {
+		return fechaNacto;
+	}
+
+
+
+	public void setFechaNacto(GregorianCalendar fechaNacto) {
+		this.fechaNacto = fechaNacto;
+	}
+
+
+
+	public Sexo getSexo() {
+		return sexo;
+	}
+
+
+
+	public void setSexo(Sexo sexo) {
+		this.sexo = sexo;
+	}
+
+
+
 	public String getFechaCorta() {
 		 
 	}
 	
 	public int getEdad() {
-		
+		Calendar fechaNacimiento = this.fechaNacto;
+		Calendar fechaActual = Calendar.getInstance();
+		int edad = fechaActual.get(Calendar.YEAR) - fechaNacimiento.get(Calendar.YEAR);
+		return edad;
 	}
 		
 }
